873efb2c9161f0da96268d1695474d679b1553f5
mrg327 <matthewghere327@gmail.com>
2025-01-20 22:30:30 +0000

    Video test successful

--- lib/test_video.py
+++ lib/test_video.py
@@ -1,30 +1,41 @@
 import cv2
 
+"""
+This is an example file taken from GeeksForGeeks.com to demo
+streaming in the camera feed.
+"""
+
 # Open the default camera
-cam = cv2.VideoCapture(0)
+CAM = cv2.VideoCapture(0)
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter("output.mp4", fourcc, 20.0, (frame_width, frame_height))
+def preview_video():
+    # Get the default frame width and height
+    frame_width = int(CAM.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(CAM.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-while True:
-    ret, frame = cam.read()
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter("output.mp4", fourcc, 20.0, (frame_width, frame_height))
 
-    # Write the frame to the output file
-    out.write(frame)
+    while True:
+        ret, frame = CAM.read()
 
-    # Display the captured frame
-    cv2.imshow("Camera", frame)
+        # Write the frame to the output file
+        out.write(frame)
 
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) == ord("q"):
-        break
+        # Display the captured frame
+        cv2.imshow("Camera", frame)
 
-# Release the capture and writer objects
-cam.release()
-out.release()
-cv2.destroyAllWindows()
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    # Release the capture and writer objects
+    CAM.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    preview_video()
